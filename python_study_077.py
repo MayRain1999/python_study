efadb20cbc86bfb9c5c69c36b1a700610c19886c
Xiaoping027 <zhangxiaoping0207@gmail.com>
2017-08-20 21:54:09 +0800

    python_study 成绩排序

--- python_study_077.py
+++ python_study_077.py
@@ -4,3 +4,18 @@
 @file:python_study_077.py
 @time:2017/8/20  21:31
 """
+while True:
+    try:
+        a = input()
+        b = input()
+        c = list()
+        for i in range(a):
+            c.append(raw_input())
+        if b == 0:
+            for i in sorted(c, key=lambda a: int(a.split()[1]), reverse=True):
+                print i
+        else:
+            for i in sorted(c, key=lambda a: int(a.split()[1])):
+                print i
+    except:
+        break
