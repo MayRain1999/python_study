c192b969534366f45914330362bdaa51e1a6779c
Xiaoping027 <zhangxiaoping0207@gmail.com>
2017-08-20 21:47:32 +0800

    python_study 24 点游戏算法

--- python_study_076.py
+++ python_study_076.py
@@ -8,18 +8,18 @@
 def trans(num, x):
     total = 0
     if len(x) ==1:
-        if  abs(num - x[0])<0.01:
+        if  abs(num - x[0])<0.001:
             return 1
         else:
             return 0
     else:
-        for i in range(len(x)):
+        for i in xrange(len(x)):
             a = x[i]
             b = x[:]
             b.pop(i)
-            total = total + (trans(num -a, b) +trans(num+a, b) +
-                             trans(num*a, b) + trans(num/a, b))
-            return total
+            total += trans(num -a, b) +trans(num+a, b) +\
+                    trans(num*a, b) + trans(num/a, b)
+        return total
 
 while True:
     try:
