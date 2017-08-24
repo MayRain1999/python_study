fc3ed0d4d510302832dc048a627715f2afd5662a
Xiaoping027 <zhangxiaoping0207@gmail.com>
2017-08-24 16:25:45 +0800

    python_study_086 #火车进站和出站 字典序

--- python_study_086.py
+++ python_study_086.py
@@ -4,28 +4,28 @@
 @file: python_study_086.py
 @time: 2017/8/23 11:48
 """
-def handle(count, pre_station,after_station):
+def handle(count, pre_station):
     num = 1
     for i in range(count):
         num *=i
     #循环次数
     a = num - 1
-    b = -len(start_arr) -1
-    for i in range(num):
-        for j in range(-2, b, -1):
-            if   pre_station [j] <pre_station[j+1]:
-                k  = pre_station[j]
+    b = -len(pre_station) -1
+    for item in range(a):
+        for i in range(-2, b, -1):
+            if   pre_station [i] <pre_station[i+1]:
+                k  = pre_station[i]
                 break
         x = pre_station[i+1:]
-        r = [i for i in x if i>k]
-        y = min(enumerate(r), key = lambda j:j[1])
-        x[y[0]], pre_station[j] = k, y[1]
-        after_station = pre_station[:i+1]+ x[::-1]
-        print after_station
+        r = [item for item in x if item>k]
+        y = min(enumerate(r), key = lambda i:i[1])
+        x[y[0]], pre_station[i] = k, y[1]
+        pre_station = pre_station[:i+1]+ x[::-1]
+        print pre_station
 while True:
     try:
         count = input()
-        start_arr = map(int, raw_input().split())
+        start_arr = raw_input().split()
         result  =[]
         handle(count, start_arr)
     except:
