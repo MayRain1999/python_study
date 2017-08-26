1c9d88bff1faab8b03bbcec2dea16b38864b3552
Xiaoping027 <zhangxiaoping0207@gmail.com>
2017-08-26 20:46:44 +0800

    python_study_097 # 滴滴面试题 最大连续子串和

---
+++ python_study_097.py
@@ -0,0 +1,17 @@
+# encoding: utf-8
+"""
+@author: Xiaoping
+@file: python_study_097.py
+@time: 2017/8/26 15:59
+"""
+while True:
+    try:
+        arr= map(int, raw_input().split())
+        k = int(raw_input())
+        set(arr)
+        arr.sort()
+        arr.reverse()
+        print arr
+        print arr[k-1]
+    except:
+        break
