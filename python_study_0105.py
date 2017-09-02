a5a1e49b32f8130154e0997416bd1f7ba78b9596
Xiaoping027 <zhangxiaoping0207@gmail.com>
2017-09-02 11:59:27 +0800

    python_study_0104.py  #合法IP

---
+++ python_study_0105.py
@@ -0,0 +1,22 @@
+# -*- coding:utf-8 -*-
+"""
+@author:Xiaoping
+@file:python_study_0105.py
+@time:2017/9/2  11:55
+"""
+#合法ip
+while True:
+    try:
+        inIP= raw_input().split('.')
+        flag = 0
+        for i in inIP:
+            if int(i) >= 0 and int(i) <= 255:
+                pass
+            else:
+                flag = 1
+        if flag == 0:
+            print 'YES'
+        else:
+            print 'NO'
+    except:
+        break
